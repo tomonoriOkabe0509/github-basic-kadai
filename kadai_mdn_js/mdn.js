c2dc9ba209fa4378967b9f1da45f84359851f2f7
tomonoriOkabe0509 <mmsxq831@gmail.com>
2025-03-27 20:28:40 +0900

    課題修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
-const date = new Date(2024,10,12);
+const date = new Date();
 
 const y=date.getFullYear();
-const m=date.getMonth();
+const m=date.getMonth()+1;
 const d=date.getDate();
  
 console.log(`${y}年${m}月${d}日`);
